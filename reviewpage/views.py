3765e8b3a120a703b343bd6dbc2072ec36862519
tiuln2010 <destiy@naver.com>
2019-07-27 18:10:06 +0900

    리뷰페이지 수정

--- reviewpage/views.py
+++ reviewpage/views.py
@@ -10,12 +10,13 @@ def index(request):
     if request.method == 'GET': # index
         reviews = Review.objects.all()
         return render(request, 'reviewpage/index.html', {'reviews': reviews})
+
     elif request.method == 'POST': # create
         content = request.POST['content']
         photo = request.FILES.get('photo', False)
+        
         ### 수정: 리뷰레이팅 추가
         review_rating = request.POST['review_rating']
-        ###
         Review.objects.create(review_rating=review_rating, content=content, author = request.user, photo=photo)
         return redirect('/reviews')
 
@@ -33,25 +34,21 @@ def show(request, id):
         # product, author, liked_users
 
         content = request.POST['content']
-        review_rating = request.POST['rate']
-
-        if request.FILES['photo']:
-            photo = request.FILES['photo']
-            fs = FileSystemStorage()
-            filename = fs.save(fs.name, myfile)
-            uploaded_file_url = fs.url(filename)
+        review_rating = request.POST['review_rating']
+        # photo = request.FILES['photo']:
 
         review.content = content
         review.review_rating = review_rating
 
         review.save()
         ### 수정: 리뷰 점수도 수정받기
-        if review.review_rating != request.POST['review_rating']:
-            review.review_rating = request.POST['review_rating']
-            review.save()
-            product = review.product 
-            product.get_rating()  
-            product.calc_rank_point()
+
+        # if review.review_rating != request.POST['review_rating']:
+        #     review.review_rating = request.POST['review_rating']
+        #     review.save()
+        #     product = review.product 
+        #     product.get_rating()  
+        #     product.calc_rank_point()
 
         return redirect('/reviews')
 
