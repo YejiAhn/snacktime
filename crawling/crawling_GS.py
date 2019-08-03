1d4fd4ad70838c1fb97ca8162eb0bc68f5b1ba67
Najing <rapace0215@gmail.com>
2019-08-03 15:03:43 +0900

    crawling update

--- crawling/crawling_GS.py
+++ crawling/crawling_GS.py
@@ -5,6 +5,10 @@ from bs4 import BeautifulSoup
 import re
 import csv
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+import time
+
+
 
 driver = webdriver.Chrome(executable_path="C:\Workspace\chromedriver")
 driver.implicitly_wait(3)
@@ -12,35 +16,45 @@ base_url= 'http://gs25.gsretail.com/gscvs/ko/products/youus-different-service'
 
 group = ['음료/커피', '유제품', '과자/간식', '라면/가공제품', '생활용품']
 PB = 'GS'
-category_range = list(range(len(group)))
+#category_range = list(range(len(group)))
 all_prod_list=[['id','category','image','name','price','PB']]
 prod_id=1
 
 driver.get(base_url)
-#element = driver.find_element_by_id('productDrink')
-#element.click()
-category=''
-html = driver.page_source
-soup = BeautifulSoup(html, 'html.parser')
-divs = driver.find_elements_by_class_name('prod_box')
+time.sleep(3)
+element = driver.find_element_by_id('productCookie')
+element.click()
 
-for c in divs:    
-    products = soup.select
-    prod_list= soup.find("div", {"class": "prod_box"})
+time.sleep(3)
+for n in range(16):
+    category = '과자/간식'
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    #divs = driver.find_elements_by_class_name('prod_box')
+        
+    #products = soup.select
+    prod_list= soup.find("div", {"class": "tab_cont on"})
     name_list = prod_list.select('.tit')
     price_list = prod_list.select('.cost')
     img_list = prod_list.select('.img')
     if len(name_list)==len(price_list) and len(price_list)==len(img_list):
+        
         for j in range(len(name_list)):
-            temp_list=[prod_id, category, img_list[j].img['src'], name_list[j].text, price_list[j].text, PB]
+            if str(img_list[j]).find('src') != -1:
+                img = img_list[j].img['src']
+            else:
+                print(str(img_list[j]))
+                img = ''
+                
+            temp_list=[prod_id, category, img, name_list[j].text, price_list[j].text, PB]
             prod_id=prod_id+1
             all_prod_list.append(temp_list)
+    element2 = driver.find_element_by_class_name('next')
+    element2.click()
+    time.sleep(3)
 
-#element2 = driver.find_element_by_class_name('next')
-#element2.click()
 
-
-f = open('gs''.csv', 'w', encoding='utf-8', newline='')
+f = open('gs_snack2''.csv', 'w', encoding='utf-8', newline='')
 wr = csv.writer(f)
 for info in range(prod_id):
     wr.writerow(all_prod_list[info])
